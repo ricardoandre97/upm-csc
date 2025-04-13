ec754c0d4291b7e2bc93181ffd8b38d5fc7253bb
Ricardo Gonzalez <ricardo.gonzalez@maf.ae>
2025-04-13 22:36:09 +0800

    Update q1.java

--- CSC3100/src/lab3/q1.java
+++ CSC3100/src/lab3/q1.java
@@ -23,16 +23,16 @@ public class q1 {
 		 double finalExamMarks = scanner.nextDouble();
 		
 
-		double finalMarks = ( 
+		 double finalMarks = ( 
 				(assesmementOneMarks * 0.15) +
 				(assesmementTwoMarks * 0.20) +
 				(labMarks            * 0.35) +
 				(finalExamMarks      * 0.30)
 				
 		);
-        System.out.println("Your total mark for student " + String.format("%.0f", matricNumber) + " in CSC3100 is " + String.format("%.2f", finalMarks));
-
-        scanner.close();
+	        System.out.println("Your total mark for student " + String.format("%.0f", matricNumber) + " in CSC3100 is " + String.format("%.2f", finalMarks));
+	
+	        scanner.close();
 		 
 	 }
 }
