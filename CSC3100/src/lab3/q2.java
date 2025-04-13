91d4bf8e10005c08d58cde3551dd10a59bd5cff4
Ricardo Gonzalez <ricardo.gonzalez@maf.ae>
2025-04-13 22:36:50 +0800

    Update q2.java

--- CSC3100/src/lab3/q2.java
+++ CSC3100/src/lab3/q2.java
@@ -29,31 +29,31 @@ public class q2 {
 		 double itemOneAmount = (itemOneQuantity * itemOneUnitPrice);
 		 double itemTwoAmount = (itemTwoQuantity * itemTwoUnitPrice);
 		 
-	     System.out.println("******************************************");
+	    	 System.out.println("******************************************");
   
-         System.out.println("Item 1: " + itemOne);
-         System.out.println("Quantity: " + itemOneQuantity);
-         System.out.println("Unit Price: " + itemOneUnitPrice);
-         System.out.println("Amount: " + itemOneAmount);
+	         System.out.println("Item 1: " + itemOne);
+	         System.out.println("Quantity: " + itemOneQuantity);
+	         System.out.println("Unit Price: " + itemOneUnitPrice);
+	         System.out.println("Amount: " + itemOneAmount);
 
-	     System.out.println("******************************************");
-	     
-         System.out.println("Item 2: " + itemTwo);
-         System.out.println("Quantity: " + itemTwoQuantity);
-         System.out.println("Unit Price: " + itemTwoUnitPrice);
-         System.out.println("Amount: " + itemTwoAmount);
-         
-         double subtotal = (itemOneAmount + itemTwoAmount);
-         double tax = 0.7;
-         double serviceCharge = 1.16;
-         String total = String.format("%.2f", (subtotal + tax + serviceCharge));
-         System.out.println("\nSubtotal: RM " + subtotal );
-         System.out.println("Government Tax: RM " + tax);
-         System.out.println("Service Charge: RM " + serviceCharge);
-         
-         System.out.println("\nTotal: RM " + total);
-         
-        scanner.close();
+	   	 System.out.println("******************************************");
+		     
+	         System.out.println("Item 2: " + itemTwo);
+	         System.out.println("Quantity: " + itemTwoQuantity);
+	         System.out.println("Unit Price: " + itemTwoUnitPrice);
+	         System.out.println("Amount: " + itemTwoAmount);
+	         
+	         double subtotal = (itemOneAmount + itemTwoAmount);
+	         double tax = 0.7;
+	         double serviceCharge = 1.16;
+	         String total = String.format("%.2f", (subtotal + tax + serviceCharge));
+	         System.out.println("\nSubtotal: RM " + subtotal );
+	         System.out.println("Government Tax: RM " + tax);
+	         System.out.println("Service Charge: RM " + serviceCharge);
+	         
+	         System.out.println("\nTotal: RM " + total);
+	         
+	         scanner.close();
 		 
 	 }
 }
